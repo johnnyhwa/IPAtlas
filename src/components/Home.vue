279c6d1248742d728dc77c401c8918bc3df6f3b3
johnnyhwa <hhz94@qq.com>
2017-11-14 17:24:35 +0800

    111

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="container">
-    <canvas id="mouse-screen" name=""></canvas>
-  </div>
+	<div id="container">
+		<canvas id="mouse-screen"></canvas>
+	</div>
 </template>
 
-<style scoped>
+<style>
+body {
+  background: #eee;
+}
 #container {
-  width: 1200px;
+  background: #fff;
+  width: 1402px;
   margin: 0 auto;
 }
 #mouse-screen {
@@ -15,50 +19,59 @@
 </style>
 
 <script>
+import Node from "../module/node";
 export default {
   data() {
     return {
-      c_width: 1200,
-      c_height: 600,
+      c_width: 1400,
+      c_height: 800,
       canvas: null,
       stage: null,
       con: null,
       update: true,
-      ejs: null
+      ejs: null,
+      fpsLabel: null,
+      nodePool: [],
+      nodedata: []
     };
   },
   mounted() {
     this.init();
-    this.initTransform();
     this.draw();
   },
   methods: {
     /**
 	 * @method init
-	 * @description
+	 * @description 初始化方法
 	 * @returns {void}
 	 */
     init() {
       this.canvas = document.getElementById("mouse-screen");
       this.con = new createjs.Container();
-      this.canvas.width = 1200;
-      this.canvas.height = 600;
+      this.canvas.width = this.c_width;
+      this.canvas.height = this.c_height;
       this.stage = new createjs.Stage(this.canvas);
       createjs.Touch.enable(this.stage);
       this.stage.enableMouseOver();
+
+      this.initTransform(this.con, this.stage, this.canvas);
     },
-    initTransform() {
+
+    /**
+	 * @method initTransform
+	 * @description 初始化画布缩放移动方法
+	 * @return {void}
+	 * 
+	 */
+    initTransform(con, stage, canvas) {
       var self = this;
-      var con = this.con;
-      var stage = this.stage;
-      var update = this.update;
-	  var canvas = this.canvas;
-	  
+
       canvas.oncontextmenu = function(e) {
         var e = event || window.event;
 
         return false;
       };
+
       stage.addEventListener("stagemousedown", event => {
         console.log(event);
         var cx = event.stageX;
@@ -66,22 +79,23 @@ export default {
         var sx = con.x;
         var sy = con.y;
         var scalex = con.scaleX;
-		var scaley = con.scaleY;
-		
-		// 左键框选
-        if (event.nativeEvent.button === 0) {
+        var scaley = con.scaleY;
+        // 左键框选
+        if (event.nativeEvent.button === 0 && !stage.hitTest(cx, cy)) {
           var w = 0;
           var h = 0;
           var g = new createjs.Graphics()
             .ss(1)
             .s("blue")
-            .f("rgba(5,120,255,.8)")
+            .f("rgba(10,140,255,.4)")
             .dr(cx, cy, w, h);
           var command = g.command;
           var s = new createjs.Shape(g);
           s.set({
-            alpha: 0.2
+            alpha: 0.2,
+            name: "recselecter"
           });
+          console.log(s);
           stage.addChild(s);
           stage.addEventListener("stagemousemove", e => {
             w = e.stageX - cx;
@@ -90,9 +104,9 @@ export default {
             command.h = h;
             self.update = true;
           });
-		}
-		
-		// 右键点击
+        }
+
+        // 右键点击
         if (event.nativeEvent.button === 2) {
           stage.addEventListener("stagemousemove", e => {
             con.setTransform(
@@ -106,10 +120,12 @@ export default {
         }
         stage.addEventListener("stagemouseup", evt => {
           stage.removeAllEventListeners("stagemousemove");
-          console.log(stage);
+          // 移除选框
+          stage.removeChild(stage.getChildByName("recselecter"));
+          self.update = true;
         });
-	  });
-	  
+      });
+
       // 鼠标滑轮事件
       canvas.addEventListener("mousewheel", event => {
         var dir = event.wheelDelta;
@@ -127,11 +143,22 @@ export default {
         event.preventDefault();
       });
     },
+    /**
+	 * @method draw
+	 * @description 绘制方法
+	 * @return {void}
+	 * 
+	 */
     draw() {
       var self = this;
       var con = this.con;
       var stage = this.stage;
       var arr = [];
+      this.fpsLabel = new createjs.Text("-- fps", "bold 18px Arial", "#000");
+      stage.addChild(this.fpsLabel);
+      this.fpsLabel.x = 10;
+      this.fpsLabel.y = 20;
+
       var img = new Image();
       img.src = require("../assets/images/ico_root.png");
       img.onload = handleImageLoad;
@@ -161,13 +188,31 @@ export default {
       }
 
       stage.addChild(con);
+      createjs.Ticker.framerate = 30;
       createjs.Ticker.addEventListener("tick", this.tick);
     },
+
+    /**
+	 * @method drawNode
+	 * @description 节点绘制方法
+	 * @returns {void}
+	 * 
+	 */
+    drawNode() {},
+    /**
+	 * @method tick
+	 * @description tick handle
+	 * @param {Object} event
+	 * @returns {void}
+	 * 
+	 */
     tick(event) {
+      this.fpsLabel.text =
+        Math.round(createjs.Ticker.getMeasuredFPS()) + " fps";
       if (this.update) {
         this.update = false;
         this.stage.update();
-        console.log(event);
+        //console.log(event);
       }
     }
   },
---
+++ src/module/node.js
@@ -0,0 +1,11 @@
+function Node(options) {
+    this.id = options.id || ''
+    this.name = options.name || ''
+    this.type = options.type || ''
+    this.graphic = options.graphic || ''
+}
+
+Node.prototype = {
+
+}
+export default Node
